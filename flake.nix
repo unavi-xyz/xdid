c03a2af16d7da525d0624c48429184a3afadb0e2
Kayh <basedlainpilled@gmail.com>
2024-10-20 16:36:21 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  inputs = {
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      localSystem:
+      let
+        pkgs = import nixpkgs {
+          inherit localSystem;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          strictDeps = true;
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { pname = "deps"; });
+
+        cargoClippy = craneLib.cargoClippy (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            pname = "clippy";
+          }
+        );
+
+        cargoDoc = craneLib.cargoDoc (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            pname = "doc";
+          }
+        );
+      in
+      {
+        checks = {
+          inherit cargoClippy cargoDoc;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${localSystem};
+          packages = with pkgs; [
+            cargo-machete
+            cargo-release
+            nodePackages.prettier
+            rust-analyzer
+          ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
